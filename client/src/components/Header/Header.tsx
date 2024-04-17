86896a1dec8393e7e14776b351f3024393c2c9fd
zweroboy1 <fprognoz@gmail.com>
2024-04-17 18:08:05 +0300

    feat: add header and basic styles for it

--- client/src/components/Header/Header.tsx
+++ client/src/components/Header/Header.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './Header.scss';
 
 
 export const Header: React.FC = () => {
@@ -7,22 +8,23 @@ export const Header: React.FC = () => {
   const user = false;
 
   return (
-    <>
-      <div>Header</div>
+    <header className="header">
+      <div className="header__container">
       {user ? (
-        <Link to="/logout">
+        <Link to="/logout" className='button'>
           Sign Out
         </Link>
       ) : (
         <>
-          <Link to="/login">
-            Login
+          <Link to="/login" className='button'>
+            Log In
           </Link>
-          <Link to="/register">
+          <Link to="/register" className='button'>
             Sign Up
           </Link>
         </>
       )}
-    </>
+      </div>
+    </header>
   );
 };
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,8 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { Router } from './router/router.tsx';
 
-import './App.scss';
-import './index.css';
+import './styles/index.scss';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>

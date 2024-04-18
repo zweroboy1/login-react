8aafd3def5959708d0707cf94d23a8f63384dd42
zweroboy1 <fprognoz@gmail.com>
2024-04-18 16:55:21 +0300

    feat: add deals component

---
+++ client/src/components/Deals/Deals.tsx
@@ -0,0 +1,91 @@
+import React from 'react';
+import './Deals.scss';
+
+import deal1Image from '../../assets/deal1.jpg';
+import deal2Image from '../../assets/deal2.jpg';
+import deal3Image from '../../assets/deal3.jpg';
+import deal4Image from '../../assets/deal4.jpg';
+
+type deal = {
+  name: string;
+  price: number;
+  yield: number;
+  sold: number;
+  ticket: number;
+  daysLeft: number;
+  image: string;
+};
+
+const deals: deal[] = [
+  {
+    name: 'The Marina Torch',
+    price: 6500000,
+    yield: 9.25,
+    sold: 75,
+    ticket: 60000,
+    daysLeft: 150,
+    image: deal1Image,
+  },
+  {
+    name: 'HHHR Tower',
+    price: 6500000,
+    yield: 9.25,
+    sold: 75,
+    ticket: 60000,
+    daysLeft: 150,
+    image: deal2Image,
+  },
+  {
+    name: 'Ocean peaks',
+    price: 6500000,
+    yield: 9.25,
+    sold: 75,
+    ticket: 60000,
+    daysLeft: 150,
+    image: deal3Image,
+  },
+  {
+    name: 'Al Yaqoub Tower',
+    price: 6500000,
+    yield: 9.25,
+    sold: 75,
+    ticket: 60000,
+    daysLeft: 150,
+    image: deal4Image,
+  },
+];
+
+export const Deals: React.FC = () => {
+  return (
+    <section className="deal">
+      <h2 className="deal__title">Open Deals</h2>
+      <div className="deal__container">
+        {deals.map((deal) => (
+          <div className="deal__card" key={deal.name}>
+            <div className="deal__wrapper">
+              <div className="deal__image-container">
+                <img className="deal__image" src={deal.image} alt={deal.name} />
+              </div>
+              <div className="deal__content">
+                <h3 className="h3">{deal.name}</h3>
+                <div className="deal__info text">
+                  <div className="deal__info-part">
+                    {deal.price.toLocaleString()} Dhs
+                  </div>
+                  <div className="deal__info-part">Yield {deal.yield}%</div>
+                  <div className="deal__info-part">Sold {deal.sold}%</div>
+                  <div className="deal__info-part">
+                    Tiket - {deal.ticket.toLocaleString()} Dhs
+                  </div>
+                  <div className="deal__info-part">
+                    Days left {deal.daysLeft}
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </section>
+  );
+};
--- client/src/pages/Home/Home.tsx
+++ client/src/pages/Home/Home.tsx
@@ -1,21 +1,29 @@
 import React from 'react';
 import './Home.scss';
+import { Deals } from '../../components/Deals/Deals';
 
 export const Home: React.FC = () => {
   return (
     <main className="main home">
       <div className="home__wrapper">
-        <div className="home__container">
-          <h2 className="h2">The chemical &nbsp;negatively charged</h2>
-          <p className="home__text">
-            Numerous calculations predict, and experiments confirm, that the
-            force field reflects the beam, while the mass defect is not formed.
-            The chemical compound is negatively charged. Twhile the mass defect
-            is
-          </p>
-          <p className="home__button-conteiner"><a href="#!" className="button button_home">Get Started</a></p>
+        <div className="home__overlay">
+          <div className="home__container">
+            <h2 className="h2">The chemical &nbsp;negatively charged</h2>
+            <p className="home__text">
+              Numerous calculations predict, and experiments confirm, that the
+              force field reflects the beam, while the mass defect is not
+              formed. The chemical compound is negatively charged. Twhile the
+              mass defect is
+            </p>
+            <p className="home__button-conteiner">
+              <a href="#!" className="button button_home">
+                Get Started
+              </a>
+            </p>
+          </div>
         </div>
       </div>
+      <Deals />
     </main>
   );
 };

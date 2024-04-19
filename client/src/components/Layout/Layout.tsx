bbb99d56c5308b1a657e55113a863e6df9936756
zweroboy1 <fprognoz@gmail.com>
2024-04-19 22:35:47 +0300

    feat: add button "Main" to the header

--- client/src/components/Layout/Layout.tsx
+++ client/src/components/Layout/Layout.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Outlet } from 'react-router-dom';
+import { Link, Outlet } from 'react-router-dom';
 import { Header } from '../Header/Header';
 import { ToastContainer } from 'react-toastify';
 
@@ -8,6 +8,7 @@ import 'react-toastify/dist/ReactToastify.css';
 export const Layout: React.FC = () => {
   return (
     <div className="wrapper">
+      <Link to="/" className="logo button button_header">Main</Link>
       <ToastContainer
         limit={1}
         autoClose={3000}
